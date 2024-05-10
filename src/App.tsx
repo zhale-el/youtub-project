4a2c54fa690645988d61ab3a9ec7a0f0d0c71a93
zhaleh <zhalehelmi@gmail.com>
2024-05-10 21:04:09 +0330

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PageHeader from "./layouts/pageHeader/PageHeader";
+
+const App = () => {
+  return (
+    <div className="max-h-screen flex flex-col">
+      <PageHeader />
+      <div>2</div>
+      <div>3</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { cva } from "class-variance-authority";
+
+const buttonStyles = cva(["hover:bg-secondary-hover", "transition-colors"], {
+  variants: {
+    variant: {
+      default: ["bg-secondary"],
+      ghost: [],
+    },
+    size: {
+      default: ["rounded", "p-2"],
+      icon: [
+        "rounded-full",
+        "w-10",
+        "h-10",
+        "flex",
+        "items-center",
+        "justify-center",
+        "p-2.5",
+      ],
+    },
+  },
+});
+
+const classes = buttonStyles({ size: "icon" });
+
+const Button = () => {
+  return <button />;
+};
+
+export default Button;
